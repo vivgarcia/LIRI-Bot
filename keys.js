dc9f8aa654606c9cfb5cdf5cbe4ccb84b65db59c
vivgarcia <viv.garcia88@gmail.com>
2020-04-02 14:43:25 -0500

    adding spotify keys

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.5dd3715c321744cd80c62037b72faacf,
+  secret: process.env.744f300900d747a1b9d7ef8745f5c7e2
 };
